9f22ff70615047a4f39655cca26c01358bbad5e0
Ypolites <manon.bonnafous@outlook.com>
2024-05-06 16:16:26 -0400

    créationd e la route /year

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/year', (req, res) {
+  const newDate = Date.now();
+  res.json({ year: newDate })
+});
+
 module.exports = router;
